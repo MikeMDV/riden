abf81ae426cb4de56b7aae6eb836fba0333b4289
MikeMDV <mmarven09@gmail.com>
2026-04-12 15:49:24 -0400

    Adding new mock client

---
+++ src/python/mockclient/mockclient.py
@@ -0,0 +1,7 @@
+"""Script to connect to the websocketserver as a mock client and send and receive messages"""
+
+def main():
+    """Function to handle execution"""
+
+if __name__ == "__main__":
+    main()
